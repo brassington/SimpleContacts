254c4d543d392d76a2567f06c24773becab98e2b
J. Andrew Brassington <jabbrass@zoho.com>
2013-09-08 22:28:03 -0500

    Added extra seed to contacts for DB

--- SimpleContacts.Data/Seeder.cs
+++ SimpleContacts.Data/Seeder.cs
@@ -23,13 +23,14 @@ namespace SimpleContacts.Data.Migrations
         private static void SeedContacts(SimpleContext context)
         {
             context.Contacts.AddOrUpdate(c => c.Name,
-                new Contact() { ContactId = 1, Name = "Bob Smith", PhoneNumber = "707.345.6547"},
-                new Contact() { ContactId = 2, Name = "Nick Teacher", PhoneNumber = "345.678.2345"},
-                new Contact() { ContactId = 3, Name = "Yola Franks", PhoneNumber = "567.234.5678"},
-                new Contact() { ContactId = 4, Name = "Frank Shoemaker", PhoneNumber = "345.234.6543"},
-                new Contact() { ContactId = 5, Name = "Franny Dorba", PhoneNumber = "456.324.8654"},
-                new Contact() { ContactId = 6, Name = "Tom Sellic", PhoneNumber = "934.593.6943"},
-                new Contact() { ContactId = 7, Name = "Bob Sinatra", PhoneNumber = "345.888.3456"}
+                new Contact() { ContactId = 1, Name = "Bob Smith", PhoneNumber = "707.345.6547" },
+                new Contact() { ContactId = 2, Name = "Nick Teacher", PhoneNumber = "345.678.2345" },
+                new Contact() { ContactId = 3, Name = "Yola Franks", PhoneNumber = "567.234.5678" },
+                new Contact() { ContactId = 4, Name = "Frank Shoemaker", PhoneNumber = "345.234.6543" },
+                new Contact() { ContactId = 5, Name = "Franny Dorba", PhoneNumber = "456.324.8654" },
+                new Contact() { ContactId = 6, Name = "Tom Sellic", PhoneNumber = "934.593.6943" },
+                new Contact() { ContactId = 7, Name = "Bob Sinatra", PhoneNumber = "345.888.3456" },
+                new Contact() { ContactId = 8, Name = "Diana Ross", PhoneNumber = "346.324.6432" }
                 );
         }
     }
